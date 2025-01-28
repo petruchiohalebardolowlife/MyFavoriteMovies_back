7b257e77608c159c5883b0f1da954ebe044092d4
petruchiohalebardolowlife <levinyegor2000@gmail.com>
2025-01-28 12:13:18 +0300

    Add routes for removing handlers

--- routes/routes.go
+++ routes/routes.go
@@ -12,7 +12,9 @@ func SetUpRoutes(router *gin.Engine) {
     router.PATCH("/users/:id", handlers.UpdateUser)
 	router.GET("/users/:id", handlers.ReadUser)
     router.POST("/users/:id/genres", handlers.AddFavouriteGenre)
+	router.POST("/users/:id/genres", handlers.DeleteFavouriteGenre)
     router.POST("/users/:id/movies", handlers.AddFavouriteMovie)
+	router.DELETE("/users/:id/movies", handlers.DeleteFavouriteMovie)
     router.PATCH("/users/:id/movies/toggle", handlers.ToggleWatchedStatus)
     router.POST("/genres", handlers.AddGenre)
 }
