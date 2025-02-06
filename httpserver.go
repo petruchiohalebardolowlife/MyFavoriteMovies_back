7785343b450e09c24d84432ca4b235ed42272db4
petruchiohalebardolowlife <levinyegor2000@gmail.com>
2025-02-06 22:39:03 +0300

    Add GQL Scheme, 2 resolvers

---
+++ httpserver.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"myfavouritemovies/routes"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateServer() *gin.Engine {
+  router := gin.Default()
+  router.Use(sessions.Sessions("my_session", cookie.NewStore([]byte("secret"))))
+  routes.SetUpRoutes(router)
+  return router
+}
